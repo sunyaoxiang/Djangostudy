4f974bcfb69d7687dcc95e18c72956dd177a172e
yaoxiang.sun <yaoxiang.sun@datayes.com>
2017-11-17 19:32:32 +0800

    https://github.com/7sDream/django-intro-zh/blob/master/docs/part5.md

--- mysite_project/polls/models.py
+++ mysite_project/polls/models.py
@@ -13,15 +13,14 @@ from django.utils import timezone
 # @python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length = 300)
-    pub_date = models.DateField('date published')
-    pub_time = models.TimeField()
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        now = timezone.now()
-        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+        now = timezone.now()- datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=10)<= self.pub_date <=  now
 
 
 # @python_2_unicode_compatible
@@ -29,7 +28,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
-    cho_date = models.DateField('date choice')
+    cho_date = models.DateTimeField('date choice')
 
     def __str__(self):
         return self.choice_text
